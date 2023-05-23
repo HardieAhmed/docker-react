efc22c10bb2cf67492c4fd3ef9c28a34fafd2d2e
Hardie Ahmed <hardie.tariq@hotmail.com>
2023-05-23 21:56:18 +0300

    changing feature branch content

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there.
-          It's working.
+          Changing text in the feature branch.
         </p>
         <a
           className="App-link"
